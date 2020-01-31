e0683938195a566dfc1d20568bf56b663a6250e4
Obinna Nnodim <obiknows88@gmail.com>
2020-01-31 08:14:49 -0700

    changed the descriptions and made the tags hover gold.

--- src/components/bio.js
+++ src/components/bio.js
@@ -28,9 +28,9 @@ function Bio() {
               Written by <strong>{author}</strong> who lives and works in Osaka
               building useful things.
               {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
+              {/* <a href={`https://twitter.com/${social.twitter}`}>
                 You should follow him on Twitter
-              </a>
+              </a> */}
             </p>
           </section>
         )
