a0ec731e125aaa87a884263e53f1a7af6336e3c1
Obinna Nnodim <obiknows88@gmail.com>
2020-01-31 08:14:49 -0700

    changed the descriptions and made the tags hover gold.

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: `It's Obinna`,
   shortName: `It's Obinna`,
-  description: 'A blog about all things wealth, health, tech, design, and anything interesting',
+  description: `A blog about all things music, wealth, health, tech, and anything else that's interesting`,
   url: 'https://itsobinna.com', // Domain of your site without prefix!
   prefix: '/',
   author: '@itsobinna',
